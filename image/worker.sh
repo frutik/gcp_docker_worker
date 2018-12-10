5081b63358dcf2bb5a9e02057f6aa4fd2d5ab83e
Andrew Kornilov <frutik@gmail.com>
2018-12-10 19:20:53 +0100

    Update worker.sh

--- image/worker.sh
+++ image/worker.sh
@@ -4,7 +4,15 @@ source /opt/worker.env
 
 gcloud logging write docker-worker "Docker task ${TASK_ID} started" --severity=WARNING
 
-RESULT=`su - ubuntu -s /bin/bash -c "source /opt/google-cloud-sdk/path.bash.inc && docker-credential-gcr configure-docker >/dev/null 2>&1 && docker run $TASK_IMAGE $TASK_ARGS"`
+SHARED_FILES="/tmp/worker"
+
+mkdir ${SHARED_FILES}
+
+if [ "$SHARED_BUCKET" != "$T2" ]; then
+  gsutil rsync -r -d  gs://${SHARED_BUCKET}/ ${SHARED_FILES}
+fi
+
+RESULT=`su - ubuntu -s /bin/bash -c "source /opt/google-cloud-sdk/path.bash.inc && docker-credential-gcr configure-docker >/dev/null 2>&1 && docker run -v ${SHARED_FILES}:${SHARED_FILES} $TASK_IMAGE $TASK_ARGS"`
 # control return code? or handle errors inside app?
 
 echo $RESULT
